1047c47b3f39c1bf1df23c5a4fd67efe96df9db9
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-07 22:39:32 -0700

    feat: initial setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env/index.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: env.PORT })
