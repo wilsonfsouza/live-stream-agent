248768bc10b41b63ba95b3c9b1c18f6818826893
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-07 23:26:10 -0700

    feat(database): add db seed to prepopulate rooms table with fake data

---
+++ src/database/seed.ts
@@ -0,0 +1,22 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sqlClient } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  }
+})
+
+await sqlClient.end()
+
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('Database seeded')
