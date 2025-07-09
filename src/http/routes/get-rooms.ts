2169d0fcf0e21ed7c2378a3bf4a85d08afba9e40
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-09 11:27:23 -0700

    feat(routes/get-rooms): add SQL limit/offset pagination

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,22 +1,41 @@
 import { count, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
 import { db } from '../../database/connection.ts'
 import { schema } from '../../database/schema/index.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
-  app.get('/rooms', async () => {
-    const results = await db
-      .select({
-        id: schema.rooms.id,
-        name: schema.rooms.name,
-        createdAt: schema.rooms.createdAt,
-        questionsCount: count(schema.questions.id),
-      })
-      .from(schema.rooms)
-      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
-      .groupBy(schema.rooms.id)
-      .orderBy(schema.rooms.createdAt)
+  app.get(
+    '/rooms',
+    {
+      schema: {
+        querystring: z.object({
+          page: z.coerce.number().default(1),
+          pageSize: z.coerce.number().default(5),
+        }),
+      },
+    },
+    async (request) => {
+      const { page, pageSize } = request.query
 
-    return results
-  })
+      const results = await db
+        .select({
+          id: schema.rooms.id,
+          name: schema.rooms.name,
+          createdAt: schema.rooms.createdAt,
+          questionsCount: count(schema.questions.id),
+        })
+        .from(schema.rooms)
+        .leftJoin(
+          schema.questions,
+          eq(schema.questions.roomId, schema.rooms.id)
+        )
+        .groupBy(schema.rooms.id)
+        .orderBy(schema.rooms.createdAt)
+        .limit(pageSize)
+        .offset((page - 1) * pageSize)
+
+      return results
+    }
+  )
 }
