50400d5736be905456cb73fedac107bcf3047e56
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-09 10:35:49 -0700

    feat(routes/get-rooms): added createdAt and questions count to the return of this endpoint

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,3 +1,4 @@
+import { count, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../database/connection.ts'
 import { schema } from '../../database/schema/index.ts'
@@ -8,8 +9,12 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt)
 
     return results
