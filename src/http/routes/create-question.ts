3ce3c3f1a169042be082bb60b64a64b5ee3d3c8f
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-10 14:37:52 -0700

    feat(http/create-question): compare similarity between question and audio chunks for semantic search to then generate answer to user

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import z from 'zod/v4'
 import { db } from '../../database/connection.ts'
 import { schema } from '../../database/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -24,11 +26,41 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params
       const { question } = request.body
 
+      const embeddings = await generateEmbeddings(question)
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
+
       const result = await db
         .insert(schema.questions)
         .values({
           roomId,
           question,
+          answer,
         })
         .returning()
 
@@ -38,7 +70,10 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
         throw new Error('Failed to create a new question in room')
       }
 
-      return reply.status(201).send({ questionId: createdQuestion.id })
+      return reply.status(201).send({
+        questionId: createdQuestion.id,
+        answer,
+      })
     }
   )
 }
