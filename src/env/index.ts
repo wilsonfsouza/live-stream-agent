de3a65b36f3e762feb51bd3119d536b3746814d2
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-10 14:35:36 -0700

    feat(env): add gemini env variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,8 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.url().startsWith('postgresql://'),
   GEMINI_API_KEY: z.string(),
-  GEMINI_MODEL: z.string(),
+  GEMINI_AUDIO_TRANSCRIPTION_MODEL: z.string(),
+  GEMINI_GENERATE_EMBEDDINGS_MODEL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
