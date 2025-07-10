8a2875275b482f75fd9e235f665959720dde990b
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-10 14:31:44 -0700

    feat(services/gemini): add services to generate embeddings and answer

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -5,7 +5,7 @@ const gemini = new GoogleGenAI({
   apiKey: env.GEMINI_API_KEY,
 })
 
-const model = env.GEMINI_MODEL
+const model = env.GEMINI_AUDIO_TRANSCRIPTION_MODEL
 
 export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
   const response = await gemini.models.generateContent({
@@ -25,6 +25,64 @@ export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
 
   if (!response.text) {
     throw new Error('It was not possible convert the audio')
+  }
+
+  return response.text
+}
+
+export async function generateEmbeddings(text: string) {
+  const response = await gemini.models.embedContent({
+    model: env.GEMINI_GENERATE_EMBEDDINGS_MODEL,
+    contents: [{ text }],
+    config: {
+      taskType: 'RETRIEVAL_DOCUMENT',
+    },
+  })
+
+  const embeddings = response.embeddings?.[0].values
+
+  if (!embeddings) {
+    throw new Error('It was not possible to generate embeddings')
+  }
+
+  return embeddings
+}
+
+export async function generateAnswer(
+  question: string,
+  transcriptions: string[]
+) {
+  const context = transcriptions.join('\n\n')
+
+  const prompt = `
+    Using the text provided below as a context, reply to the question clearly and precisely, using US English. 
+
+    CONTEXT:
+    ${context}
+
+    QUESTION:
+    ${question}
+
+    INSTRUCTIONS:
+    - Only use information from the provided context;
+    - If an answer is not found in the context, only reply that you do not have enought information to answer the question;
+    - Be objective;
+    - Keep an educative and professional tone;
+    - Reference segments relevants from the context, if appropriate;
+    - If you are going to reference the context, use the term "content from class";
+  `.trim()
+
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: prompt,
+      },
+    ],
+  })
+
+  if (!response.text) {
+    throw new Error('Failed to generate answer by Gemini')
   }
 
   return response.text
