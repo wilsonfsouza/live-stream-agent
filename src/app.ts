42c94d8f6e09bf3ca4f49d1ac9ca1717da729195
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-04 23:14:05 -0800

    feat: add HOST_URL and build setup

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,35 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastifyMultipart } from "@fastify/multipart";
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { createQuestionRoute } from "./http/routes/create-question.ts";
-import { createRoomRoute } from "./http/routes/create-room.ts";
-import { getRoomAudioCheckRoute } from "./http/routes/get-room-audio-check.ts";
-import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts";
-import { getRoomRoute } from "./http/routes/get-room.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
-import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
+} from 'fastify-type-provider-zod'
+import { env } from './env/index.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomRoute } from './http/routes/get-room.ts'
+import { getRoomAudioCheckRoute } from './http/routes/get-room-audio-check.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
-export const app = fastify().withTypeProvider<ZodTypeProvider>();
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
-});
+  origin: env.HOST_URL,
+})
 
-app.register(fastifyMultipart);
+app.register(fastifyMultipart)
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-app.register(getRoomsRoute);
-app.register(createRoomRoute);
-app.register(getRoomQuestionsRoute);
-app.register(createQuestionRoute);
-app.register(uploadAudioRoute);
-app.register(getRoomRoute);
-app.register(getRoomAudioCheckRoute);
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestionsRoute)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
+app.register(getRoomRoute)
+app.register(getRoomAudioCheckRoute)
